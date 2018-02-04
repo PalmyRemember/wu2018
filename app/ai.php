521a54f27dcb3b164bba26acc2fc8b696db38765
Asus_laptop\Asus <Palmy_kiss8@hotmail.com>
2018-02-04 10:54:12 +0700

    add test res

--- app/ai.php
+++ app/ai.php
@@ -67,16 +67,16 @@ class AI
     public static function getRudeWords($text)
     {
         $bf = array("อีเหี้ย","ไอควาย","ไอสัส","เย็ดแม่ม","ช้างเย็ด","จันไร");
-
+        $result = array();
         for($i=0;$i<sizeof($bf);$i++){
             
             if(strpos($text, $bf[$i]) !== false){                
                 array_push($result,$bf[i]);
-                return $result;
+                
             }
         }
 
-        
+        return $result;
     }
 
     /**
